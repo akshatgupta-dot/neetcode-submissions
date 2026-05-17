4316006866d03e3adb8436f0fe60162f66dd1aac
akshatgupta-dot <akshat.0527gupta@gmail.com>
2026-05-17 14:31:56 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0; i<n; i++) {
+            bool flag=true;
+            for(int j=0; j<n; j++) {
+                if(i!=j && nums[i]==nums[j]) {
+                    flag=false;
+                    break;
+                } 
+            }
+            if(flag) {
+                return nums[i];
+            }
+        }
+    }
+};
